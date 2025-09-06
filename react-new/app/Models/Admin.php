334915daca8295e674cb06babfe8035d44a36e4a
atapasha <atapashazade@gmail.com>
2025-09-06 08:54:09 +0330

    lanaproject start with laravel and reac

---
+++ react-new/app/Models/Admin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+ class Admin extends Authenticatable
+{
+    use HasFactory,HasApiTokens,Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone',
+        'address',
+        'status',
+        'type'
+    ];
+
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+}
---
+++ react-new/database/migrations/2025_09_05_110337_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Brand;
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->text('summery');
+            $table->tinyInteger('stock')->unsigned();
+            $table->double('price');
+            $table->double('discounted_price');
+            $table->text('images');
+            $table->foreignIdFor(Category::class)->constrained();
+            $table->foreignIdFor(Brand::class)->constrained();
+            $table->enum('status', ['Active', 'Inactive'])->default('Active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
